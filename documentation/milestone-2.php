18fd3c0aa93660aff5ce80c1cf123315e573a5f4
Jason Griffin <jasonmichaelgriffin@gmail.com>
2015-05-04 01:16:11 -0600

    Completed but untested as I'm unable to upload to my personal URL.
    Also, the flow charts aren't being recognized by phpStorm to be uploaded to Github.  There is a command on Windows to get phpStorm to recognize new files (rather than just show them as red) but I can't find the equivalent on Apple.

---
+++ documentation/milestone-2.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>PWP Milestone 2</title>
+	</head>
+	<body>
+
+
+		<h1><strong>PWP Milestone 2 Assignment</strong></h1>
+		<p>             Created by:     Jason Griffin<br/>
+		             Created for:    Deep Dive Coding Bootcamp PWP Milestone 2<br/>
+		             Date:           May 3, 2015
+		             URL:            griffinwebtechnology.com
+		</p>
+
+
+		<h2><strong>Overview</strong></h2>
+
+		<p>griffinwebtechnology.com will be a professional site which will define my public facing business persona.
+		It will be clutter free, to the point, and buzzword compliant.  It might even have a little “pop” in the Oatmeal
+		sense of the word (probably not though).  The primary two audiences are potential clients and industry contacts
+		(possible collaborators). The site is intended to provide a general overview of my services, experience,
+		personality, and entice people with potential work to contact me.<br/>
+			<br/></p>
+
+		<h2><strong>Personas</strong></h2>
+
+		<h3><strong>"Industry Contact"</strong></h3>
+		<ul>
+			<li>Name: William Lee</li>
+			<li><strong>Age:</strong> 37</li>
+			<li><strong>Profession:</strong>  Freelance Backend Developer</li>
+			<li><strong>Technology:</strong>  Platform agnostic, he has a myriad of computers, nearly all self constructed,
+				as well as a few bare bones PC and Mac laptops.  Changes phones annually and is currently on a Samsung
+				Galaxy S5 which he’s not very impressed with.  Drives a Lexus LS 460 as his daily driver and a beat up old
+				Toyota 4Runner on the weekends; he wants his tools to do be specialized and good at their intended purpose.
+			<li><strong>Attitudes and Behaviors:</strong>  Friendly but introverted he is a skilled developer with more
+				work than he can handle.  With no effort he attracts clients based solely on his long established
+				reputation.  An avid outdoorsman his personal interests have moved on from consumer electronics & computers
+				to hiking and camping.  He uses reddit and facebook but doesn’t “get” instagram, vine, twitter, etc.</li>
+			<li><strong>Frustrations and Needs:</strong>  Mr Lee is frustrated at having to turn down potential clients but
+				not willing to put in more than 50 hours of work a week.  He needs to find a way to leverage the potential
+				revenue stream these potential clients represent without taking on massive amounts of work.</li>
+			<li><strong>Goals:</strong>  Needs to subcontract project management & strategy work to someone with deep
+				business experience, knowledge of full stack development, and a professional demeanor.  Wants to focus on
+				just backend development and have someone else manage the clients as well as subcontracted
+				developers/designers.</li>
+		</ul>
+
+		<h3><strong>"Potential Client"</strong></h3>
+		<ul>
+			<li>Doug E. Fresh</li>
+			<li><strong>Age:</strong> 46</li>
+			<li><strong>Profession:</strong>  Coffee Roaster (small business owner)</li>
+			<li><strong>Technology:</strong>  Owns several PC’s, using two laptops at the roastery and one for home use.
+				Also uses an iPad and iPhone 5.  Is a little techno-savvy and has put together his own bare bones
+				e-commerce website.  Is fully aware of his technology shortcomings and acknowledges the site is clunky,
+				ugly, dangerously insecure, unscalable, and does not represent what his company is all about.</li>
+			<li><strong>Attitudes and Behaviors:</strong>  This is his first foray into business outside the corporate
+				world.  Though interested in fiscal success the business is primarily a passion project.  He wants to do
+				small batch roasting with an obsessive focus on quality.  He gets up very early, works hard into the
+				mid-afternoon, spends evenings with his family and is in bed by 9pm.  Though an avid twitter and Instagram
+				user he dismisses Facebook entirely.</li>
+			<li><strong>Frustrations and Needs:</strong>  Doug is spending far more time running the business than he
+				would like.  Though moderately successful nearly all his sales are wholesale and hence he is unable to
+				justify hiring employees.  Worse, wholesale clients require large volume roasting and his passion is small
+				volume.  The direction his business is going has potential for success but it won’t feed his passion. He
+				works from 6am to 3pm Monday through Saturday.  Due to the nature of a wholesale business model only about
+				one quarter of that time is spent roasting with the rest spent running the business, doing deliveries,
+				pursuing local sales opportunities, etc.  He needs to shift his business model to focus instead on
+				increased profit margins by selling direct to consumers at full retail prices.  Ideally he would roast
+				coffee to order and ship it out within one business day of a sale.  To do this he must improve his website
+				and encourage online sales.</li>
+			<li><strong>Goals:</strong>  Wants a modern “sexy” ecommerce site which educates the consumer on the quality
+				of his product and drives sales.  As a long term goal he wants over 80% of all sales to be online.  He has
+				a huge (to him) budget of $20k for a new website (including one years maintenance).</li>
+		</ul>
+
+		<h2><strong>Use Cases</strong></h2>
+
+		<h3><strong>"Industry Contact"</strong></h3>
+
+		<p>Mr. Lee already knows me socially but not professionally.  My name pops up when asking around for freelance
+			project managers & developers.  He has a list of about 20 potential candidates he is looking at.  None of
+			these candidates are aware he is even looking for people to work with.<br/>
+			While eating breakfast at his kitchen counter he looks me up on Facebook and visits my site via a link on my
+			profile.  He’s using an old 1st gen MacBook Air.  He has looked at about a dozen people already.<br/>
+			Pulling up my home page he reads through the services I provide, notes the design and overall tone of the
+			site, then navigates to the “about” page via a two link menu at the top of the page.  Skimming through my
+			biography he scrutinizes my work history.  He notices there is not a portfolio but as that lines up with my
+			described work history it doesn’t alarm him.  Seeing no red flags his curiosity is piqued and he navigates to
+			the “contact” page, again via the menu at the top of the page.  He then writes me a direct e-mail using my
+			address on the contact page asking to set up an appointment.<br>
+		</p>
+
+		<h3><strong>"Potential Client"</strong></h3>
+
+		<p>Having met me at a local coffee enthusiasts gathering Mr. Fresh already has my card.<br/>
+			At home after dinner he is discussing with his wife the need for an improved web presence and remembers my
+			card.  Not knowing much about me other than my love of coffee and that I do something related to web
+			technology he digs out my card and navigates to my site using iPad “Retina” (aka iPad 3).<br/>
+			He carefully reads over my services and liking what he sees navigates straight to the “contact” page via the
+			two item menu at the top of the page.  Once there he clicks on my e-mail address (or pokes, not sure what you
+			call it with a tablet), the iPad automatically opens a new e-mail which he fills out asking to set up an
+			appointment<br/>
+		</p>
+
+		<h2><strong>Flow Charts</strong></h2>
+		<h3>Industry Contact Flow</h3>
+		<figure>
+			<img src="../images/contact_flow_pwp2.svg" alt="Industry Contact Use Case Flow Chart">
+		</figure>
+		<h3>Potential Client Flow</h3>
+		<figure>
+			<img src="../images/client_flow_pwp2.svg" alt="Potential Client Use Case Flow Chart">
+		</figure>
+
+		<h2><strong>Proposed Sitemap</strong></h2>
+		<h3>Homepage</h3>
+		<ul>
+			<li>A byline(sustainable scalable solutions or something similar)</li>
+			<li>Brief paragraph about my business philosophy (the whole “we take a human approach to solving your
+				technological problems by invoking synergy between your business needs and the web…” schtick but honest,
+				serious, to the point, and relatively buzzword free.</li>
+			<li>List of services (very very high level)</li>
+		</ul>
+
+		<h3>About</h3>
+		<ul>
+			<li>About Me Statement: who am I, what do I bring, what’s my vision/passion. </li>
+			<li>Resume:  A paired down “teaser” of my work history, skills, etc.</li>
+			<li>Portfolio:  Currently non-existent.  Will create/update once I have at least two items to include.</li>
+		</ul>
+
+		<h3>Contact</h3>
+		<ul>
+			<li>Short blurb essentially saying anyone can/should contact me.</li>
+			<li>Business Email.  No form, no phone number, just an e-mail address.</li>
+			<li>Links to appropriate social media sites</li>
+		</ul>
+
+
+		<h2><strong>Porposed Features and Functionality</strong></h2>
+		<ul>
+			<li>Consistent top bar nav menu and bottom contact info on each page.</li>
+			<li>On all pages a simple clickable navigation indicator (eg on the About page it would show this:
+				Home---->About) positioned at the top to the left</li>
+			<li>Consistent color and branding throughout.</li>
+			<li>A few nice effects, such as parallax, but only as appropriate and subdued as much as possible.  Just
+				enough to show I have a "nice" website but not in any way distracting</li>
+		</ul>
+	</body>
+</html>
